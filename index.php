adc227c260b366a77ab488fed5ed3766b599faf2
Ivan <100005845.joan23@fje.edu>
2019-11-05 17:42:11 +0100

    Subida directorio imagenes

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+    
+    <!-- ////////////////INICIO Header//////////////// -->
+    <div>
+        
+    </div>
+
+</body>
+</html>
