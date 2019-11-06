5c618d1ab640af5b6982ab262144b141fa160e25
AlexSalas55 <alexsalasmolina@hotmail.com>
2019-11-06 01:18:16 +0100

    Conexion PHP a BD (Query), sesiones y estilos del LogIn

--- login/index.php
+++ login/index.php
@@ -10,20 +10,37 @@
 
 	<div class="login-content">
 		<div class="ventana-login">
-		<?php
+		
+		
+		<form class="login-form" method="post" action="./services/login.proc.php" onsubmit="return login()">
+			<div class="avatar">
+				<img src="../img/img/avatar1.png" class="avatar-img">
+			</div>
+
+			<p class="mensaje" id="mensaje"></p>
+
+			<?php
 			if(isset($_REQUEST['complogin'])){
 				$complogin = $_REQUEST['complogin'];
-				echo "¡El usuario o la contraseña no son correctos!";
 			}else {
 				$complogin="";
 			}
 		?>
-		<h1>Login</h1>
-		<p class="mensaje" id="mensaje"></p>
-		<form method="post" action="./services/login.proc.php" onsubmit="return login()">
-			<input id="user" type="text" name="user" value='<?php echo $complogin ?>' placeholder="Inserta el usuario..." ><br/>
-			<input id="password" type="password" name="password" placeholder="Inserta el password" ><br/><br/>
-			<input type="submit" name="Enviar">
+
+			<label class="label-form">Email</label>
+			<input class="input-form" id="user" type="text" name="user" value='<?php echo $complogin ?>' placeholder="Inserta el usuario..." ><br/>
+			<label class="label-form">Password</label>
+			<input class="input-form" id="password" type="password" name="password" placeholder="Inserta el password..." ><br/><br/>
+			<div align="center">
+			<?php
+			if(isset($_REQUEST['complogin'])){
+				echo "¡El usuario o la contraseña no son correctos!";
+				echo "<br>";
+				echo "<br>";
+			}
+		?>
+			</div>
+			<input class="sumb-form" type="submit" name="Enviar">
 		</form>
 		</div>
 	</div>
--- login/validacion_login.js
+++ login/validacion_login.js
@@ -4,21 +4,21 @@ function login(){
 
 	if (user == '' && password == '') {
 		document.getElementById('mensaje').innerHTML = 'El campo user y password son obligatorios';
-		document.getElementById('mensaje').style.display = 'block';
+		document.getElementById('mensaje').style.display = 'flex';
 		document.getElementById('user').style.border = '2px solid red';
 		document.getElementById('password').style.border = '2px solid red';
 		return false;
 	}else if(user == ''){
 		document.getElementById('mensaje').innerHTML = 'El campo user es obligatorio';
-		document.getElementById('mensaje').style.display = 'block';
+		document.getElementById('mensaje').style.display = 'flex';
 		document.getElementById('user').style.border = '2px solid red';
-		document.getElementById('password').style.border = '2px solid #EBE9ED';
+		document.getElementById('password').style.border = '2px solid #217093';
 		return false;
 	}else if(password == ''){
 		document.getElementById('mensaje').innerHTML = 'El campo password es obligatorio';
-		document.getElementById('mensaje').style.display = 'block';
+		document.getElementById('mensaje').style.display = 'flex';
 		document.getElementById('password').style.border = '2px solid red';
-		document.getElementById('user').style.border = '2px solid #EBE9ED';
+		document.getElementById('user').style.border = '2px solid #217093';
 		return false;
 	}else{
 		return true;
