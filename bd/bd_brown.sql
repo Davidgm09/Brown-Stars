89f6774ce8f137351d9243ca56bb6a7f93c153db
Alex <alexsalasmolina@hotmail.com>
2019-11-07 18:07:40 +0100

    BD actualizada

--- bd/bd_brown.sql
+++ bd/bd_brown.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-11-2019 a las 16:43:03
+-- Tiempo de generación: 07-11-2019 a las 18:06:52
 -- Versión del servidor: 8.0.13
 -- Versión de PHP: 7.3.2
 
@@ -32,8 +32,14 @@ USE `bd_brown`;
 
 CREATE TABLE `incidencia` (
   `id_incidencia` int(11) NOT NULL,
-  `titulo_inci` varchar(20) NOT NULL,
-  `descrip_ini` text NOT NULL
+  `titulo_inci` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `descrip_ini` text NOT NULL,
+  `fecha_ini_inc` date NOT NULL,
+  `fecha_fin_inc` date DEFAULT NULL,
+  `hora_ini_inc` time NOT NULL,
+  `hora_fin_inc` time DEFAULT NULL,
+  `status_inc` enum('progress','done') NOT NULL,
+  `id_reserva` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -56,7 +62,7 @@ CREATE TABLE `recursos` (
 --
 
 INSERT INTO `recursos` (`id_recurso`, `nom_rec`, `disp_rec`, `tipo_rec`, `imagen_rec`, `desc_rec`) VALUES
-(1, 'Sala Multidisciplinària A', 'Disponible', 'Sala Multidisciplinaria', './img/recursos/SalaMultA.png', ''),
+(1, 'Sala Multidisciplinària A', 'Ocupado', 'Sala Multidisciplinaria', './img/recursos/SalaMultA.png', ''),
 (2, 'Sala Multidisciplinària B', 'Disponible', 'Sala Multidisciplinaria', './img/recursos/SalaMultB.png', ''),
 (3, 'Sala Multidisciplinària C', 'Disponible', 'Sala Multidisciplinaria', './img/recursos/SalaMultC.png', ''),
 (4, 'Sala Multidisciplinària D', 'Disponible', 'Sala Multidisciplinaria', './img/recursos/SalaMultD.png', ''),
@@ -89,7 +95,6 @@ CREATE TABLE `reserva` (
   `hora_fin_res` time DEFAULT NULL,
   `status_res` enum('progress','done') NOT NULL,
   `id_usuario` int(11) NOT NULL,
-  `id_incidencia` int(11) DEFAULT NULL,
   `id_recursos` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -97,15 +102,17 @@ CREATE TABLE `reserva` (
 -- Volcado de datos para la tabla `reserva`
 --
 
-INSERT INTO `reserva` (`id_reserva`, `fecha_ini_res`, `fecha_fin_res`, `hora_ini_res`, `hora_fin_res`, `status_res`, `id_usuario`, `id_incidencia`, `id_recursos`) VALUES
-(1, '2019-11-07', '2019-11-07', '15:16:47', '16:03:37', 'done', 1, NULL, 1),
-(2, '2019-11-07', '2019-11-07', '15:16:54', '16:11:45', 'done', 1, NULL, 3),
-(3, '2019-11-07', '2019-11-07', '15:37:46', '16:11:21', 'done', 1, NULL, 2),
-(4, '2019-11-07', '2019-11-07', '16:09:15', '16:10:39', 'done', 1, NULL, 1),
-(5, '2019-11-07', '2019-11-07', '16:10:53', '16:10:55', 'done', 1, NULL, 1),
-(6, '2019-11-07', '2019-11-07', '16:13:51', '16:37:43', 'done', 1, NULL, 5),
-(7, '2019-11-07', '2019-11-07', '16:13:58', '16:37:19', 'done', 1, NULL, 1),
-(8, '2019-11-07', '2019-11-07', '16:40:07', '16:40:13', 'done', 1, NULL, 5);
+INSERT INTO `reserva` (`id_reserva`, `fecha_ini_res`, `fecha_fin_res`, `hora_ini_res`, `hora_fin_res`, `status_res`, `id_usuario`, `id_recursos`) VALUES
+(1, '2019-11-07', '2019-11-07', '15:16:47', '16:03:37', 'done', 1, 1),
+(2, '2019-11-07', '2019-11-07', '15:16:54', '16:11:45', 'done', 1, 3),
+(3, '2019-11-07', '2019-11-07', '15:37:46', '16:11:21', 'done', 1, 2),
+(4, '2019-11-07', '2019-11-07', '16:09:15', '16:10:39', 'done', 1, 1),
+(5, '2019-11-07', '2019-11-07', '16:10:53', '16:10:55', 'done', 1, 1),
+(6, '2019-11-07', '2019-11-07', '16:13:51', '16:37:43', 'done', 1, 5),
+(7, '2019-11-07', '2019-11-07', '16:13:58', '16:37:19', 'done', 1, 1),
+(8, '2019-11-07', '2019-11-07', '16:40:07', '16:40:13', 'done', 1, 5),
+(9, '2019-11-07', '2019-11-07', '16:51:09', '16:51:34', 'done', 1, 1),
+(10, '2019-11-07', NULL, '17:02:06', NULL, 'progress', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -124,7 +131,8 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id_usuario`, `nom_usu`, `pass_usu`) VALUES
-(1, 'admin', 'admin');
+(1, 'admin', 'admin'),
+(3, 'carmupe', '1234');
 
 --
 -- Índices para tablas volcadas
@@ -134,7 +142,8 @@ INSERT INTO `usuarios` (`id_usuario`, `nom_usu`, `pass_usu`) VALUES
 -- Indices de la tabla `incidencia`
 --
 ALTER TABLE `incidencia`
-  ADD PRIMARY KEY (`id_incidencia`);
+  ADD PRIMARY KEY (`id_incidencia`),
+  ADD KEY `id_reserva` (`id_reserva`);
 
 --
 -- Indices de la tabla `recursos`
@@ -148,7 +157,6 @@ ALTER TABLE `recursos`
 ALTER TABLE `reserva`
   ADD PRIMARY KEY (`id_reserva`),
   ADD KEY `id_usuario` (`id_usuario`),
-  ADD KEY `id_incidencia` (`id_incidencia`),
   ADD KEY `id_recursos` (`id_recursos`);
 
 --
@@ -177,24 +185,29 @@ ALTER TABLE `recursos`
 -- AUTO_INCREMENT de la tabla `reserva`
 --
 ALTER TABLE `reserva`
-  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
+-- Filtros para la tabla `incidencia`
+--
+ALTER TABLE `incidencia`
+  ADD CONSTRAINT `incidencia_ibfk_1` FOREIGN KEY (`id_reserva`) REFERENCES `reserva` (`id_reserva`);
+
+--
 -- Filtros para la tabla `reserva`
 --
 ALTER TABLE `reserva`
   ADD CONSTRAINT `FK_id_recursos` FOREIGN KEY (`id_recursos`) REFERENCES `recursos` (`id_recurso`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`id_incidencia`) REFERENCES `incidencia` (`id_incidencia`),
   ADD CONSTRAINT `reserva_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`);
 COMMIT;
 
